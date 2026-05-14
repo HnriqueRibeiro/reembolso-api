7dab60425bd5fc82b213937726f3f4197c0e059f
HnriqueRibeiro <hr7.xit@gmail.com>
2026-05-14 20:20:58 -0300

    feat:autenticação do usuário

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { usersRoutes } from "./users-routes";
 import { sessionsRoutes } from "./sessions-routes";
+import { refundsRoutes } from "./refunds-routes";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 
 const routes = Router();
 
 //rotas publicas
-
 routes.use("/users", usersRoutes);
 routes.use("/sessions", sessionsRoutes);
+
+//rotas privadas
+routes.use(ensureAuthenticated);
+routes.use("/refunds", refundsRoutes);
 
 export { routes };
